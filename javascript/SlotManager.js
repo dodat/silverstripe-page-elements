bd6fbad1cbc24ac85903c459979e497f12f7bc11
Tim Klein <tim@dodat.co.nz>
2010-02-21 05:32:56 +0000

    optimisation of popup creating, setting title, displaying custom confirm message, cleanup of unused functions

--- javascript/SlotManager.js
+++ javascript/SlotManager.js
@@ -9,9 +9,8 @@
 		};
 		
 		$.fn.SlotManager.init = function(obj) {
-			var $container = $(obj);
 			
-			$.fn.SlotManager.setPopupSize($container)
+			var $container = $(obj);
 			
 			$(".handle img").mousedown(function() {
 				$(this).attr("src",
@@ -24,6 +23,7 @@
 			});
 			
 			$("div.Slot", $container).livequery(function() {
+				
 				$(this).sortable({
 					items: 'div.Element',
 					handle: 'td.handle',
@@ -47,12 +47,6 @@
 				}
 			});
 			
-		}
-		
-		$.fn.SlotManager.setPopupSize = function(container) {
-			var container_id = '#'+container.attr('id');
-			$(container_id+"_PopupHeight").attr("value", $(window).height()-100);
-			$(container_id+"_PopupWidth").attr("value", $(window).width()-150);
 		}
 		
 		
@@ -92,9 +86,10 @@ SlotManager.prototype = {
 	},
 	
 	setPopupSize: function() {
+		
 		try {
-			this.popupHeight = parseInt($(this.id + '_PopupHeight').value);
-			this.popupWidth = parseInt($(this.id + '_PopupWidth').value);
+			this.popupHeight = parseInt(window.innerHeight-100);
+			this.popupWidth = parseInt(window.innerWidth-100);
 		} catch (ex) {
 			this.popupHeight = this.defaultPopupHeight;
 			this.popupWidth = this.defaultPopupWidth;
@@ -120,19 +115,8 @@ SlotManager.prototype = {
 		} else {
 			// if clicked item is an input-element, don't trigger popup
 			var el = Event.element(e);
-			var input = Event.findElement(e,"input");
 			var tr = Event.findElement(e, "tr");
 			
-			// stop on non-found lines
-			if(tr && Element.hasClassName(tr, 'notfound')) {
-				Event.stop(e);
-				return false;
-			}
-			
-			// normal behaviour for input elements
-			if(el.nodeName == "INPUT" || input.length > 0) {
-				return true;
-			}
 			
 			try {
 				var table = Event.findElement(e,"table");
@@ -156,13 +140,7 @@ SlotManager.prototype = {
 			}
 		}
 		
-		if(this.GB_Caption) {
-			var title = this.GB_Caption;
-		} else {
-			// Getting the title from the URL is pretty ugly, but it works for now
-			type = popupLink.match(/[0-9]+\/([^\/?&]*)([?&]|$)/);
-			var title = (type && type[1]) ? type[1].ucfirst() : "";
-		}
+		var title = el.title;
 		
 		// reset internal greybox callbacks, they are not properly unregistered
 		// and fire multiple times on each subsequent popup close action otherwise
@@ -180,7 +158,50 @@ SlotManager.prototype = {
 			Event.stop(e);
 		}
 		return false;
+	},
+	/**
+	 * Deletes the given dataobject record via an ajax request
+	 * to complextablefield->Delete()
+	 * @param {Object} e
+	 */
+	deleteRecord: function(e) {
+		var img = Event.element(e);
+		var link = Event.findElement(e,"a");
+		var row = Event.findElement(e,"tr");
+		
+		// TODO ajaxErrorHandler and loading-image are dependent on cms, but formfield is in sapphire
+		var confirmed = confirm("Are you sure you want to "+link.title+"?");
+		if(confirmed)
+		{
+			img.setAttribute("src",'cms/images/network-save.gif'); // TODO doesn't work
+			new Ajax.Request(
+				link.getAttribute("href"),
+				{
+					method: 'post', 
+					postBody: 'forceajax=1' + ($('SecurityID') ? '&SecurityID=' + $('SecurityID').value : ''),
+					onComplete: function(){
+						Effect.Fade(
+							row,
+							{
+								afterFinish: function(obj) {
+									// remove row from DOM
+									obj.element.parentNode.removeChild(obj.element);
+									// recalculate summary if needed (assumes that TableListField.js is present)
+									// TODO Proper inheritance
+									if(this._summarise) this._summarise();
+									// custom callback
+									if(this.callback_deleteRecord) this.callback_deleteRecord(e);
+								}.bind(this)
+							}
+						);
+					}.bind(this),
+					onFailure: this.ajaxErrorHandler
+				}
+			);
+		}
+		Event.stop(e);
 	}
+	
 }
 
 SlotManager.applyTo('div.SlotManager');
