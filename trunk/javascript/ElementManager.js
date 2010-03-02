84b78ec7ae0d5f85b652505e4c35c103410bb802
Tim Klein <tim@dodat.co.nz>
2010-03-02 08:56:42 +0000

    early stage of historybrowser jQuery fn plugin

--- trunk/javascript/ElementManager.js
+++ trunk/javascript/ElementManager.js
@@ -4,4 +4,48 @@ jQuery(function($) {
 		$("div.ExtraFields label.left").click(function() {
 			$(this).parent().find(".middleColumn").toggle("slow");
 		});
+                
+                
+                
+                
+                
+
+	$.fn.ElementHistory = function() {
+	//	this.each(function() {
+			$.fn.ElementHistory.init(this);
+	//	});
+	};
+		
+		$.fn.ElementHistory.init = function(obj) {
+			
+			var $container = $(obj);
+                        
+                        var Element = $container.attr("id").match(/(.+)[-=_](.+)/);
+                        
+			
+                        $("table tbody tr a", $container).click(function() {
+                                var Action = $(this).attr("class");
+                                var Version = $(this).parents("tr").attr("id").match(/(.+)[-=_](.+)/);
+                                var url = "SlotManager/"+Action+"/"+Element[2]+"/"+Version[2];
+                                if(Action == "revertElement") {
+                                                if (confirm("are you sure?")) {
+                                                $.post(url);
+                                                }
+                                } else {
+                                                console.log(url);
+                                                $("iframe#ElementPreview", $container).attr("src", url);
+                                }
+                                return false;
+                        });
+                        
+                        
+		}
+		
+		
+		
+		$("div.ElementHistoryBrowser").ElementHistory();
+
+
+                
 });
+
