67470b762104a512b33ad80fca499e882f472877
Tim Klein <tim@dodat.co.nz>
2010-03-29 03:16:05 +0000

    reverted updates to jquery 1.4.2 as unresolvable conflict with combined base.js appears in live mode

--- javascript/ElementManager.js
+++ javascript/ElementManager.js
@@ -1,50 +1,60 @@
 jQuery.noConflict();
 
 jQuery(function($) {
-		
 		$("div.ExtraFields label.left").click(function() {
 			$(this).parent().find(".middleColumn").toggle("slow");
 		});
-		
-		
-		
-		$.fn.ElementHistory = function() {
-				this.each(function() {
-						$.fn.ElementHistory.init(this);
-				});
-		};
+                
+                
+                
+                
+                
+
+	$.fn.ElementHistory = function() {
+	//	this.each(function() {
+			$.fn.ElementHistory.init(this);
+	//	});
+	};
 		
 		$.fn.ElementHistory.init = function(obj) {
-				var $container = $(obj);
-				
-                var Element = $container.attr("id").match(/(.+)[-=_](.+)/);
-                
-				$("table tbody tr").mouseover(function() {
-						$(this).addClass("over");
-				}).mouseout(function() {
-						$(this).removeClass("over");
-				}).click(function(){
-						$(this).find("a.defaultaction").click();
-				});
-				
-				$("table tbody tr a", $container).click(function() {
-						var Action = $(this).attr("class");		
-						var url = $(this).attr("href");
+			
+			var $container = $(obj);
+                        
+                        var Element = $container.attr("id").match(/(.+)[-=_](.+)/);
+                        
+						$("table tbody tr").mouseover(function() {
+								$(this).addClass("over");
+						}).mouseout(function() {
+								$(this).removeClass("over");
+						}).click(function(){
+								$(this).find("a.defaultaction").click();
+						});
 						
-						if(Action == "revertElement") {
-								if (confirm("are you sure?")) {
-										$.post(url);
-								}
-						} else {
-								$("tr",$container).removeClass("current");
-								$(this).parents("tr").addClass("current");
-								$("iframe#ElementPreview", $container).attr("src", url);
-                        }
-                        return false;
-                });
+                        $("table tbody tr a", $container).click(function() {
+                                var Action = $(this).attr("class");
+                                //var Version = $(this).parents("tr").attr("id").match(/(.+)[-=_](.+)/);
+                                //var url = "SlotManager/"+Action+"/"+Element[2]+"/"+Version[2];
+								var url = $(this).attr("href");
+                                if(Action == "revertElement") {
+										if (confirm("are you sure?")) {
+												$.post(url);
+										}
+                                } else {
+                                        $("tr",$container).removeClass("current");
+										$(this).parents("tr").addClass("current");
+                                        $("iframe#ElementPreview", $container).attr("src", url);
+                                }
+                                return false;
+                        });
+                        
+                        
 		}
 		
-		$("div.ElementHistoryBrowser").ElementHistory();
 		
+		
+		$("div.ElementHistoryBrowser").ElementHistory();
+
+
+                
 });
 
